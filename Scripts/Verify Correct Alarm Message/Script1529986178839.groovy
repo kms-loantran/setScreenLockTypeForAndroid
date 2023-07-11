4c74b1353e8f0c87e2959a981df39d57f89fc5ce
loan.tran <loan.tran@FVFGX1Z8Q6LR.local>
2023-07-11 14:49:44 +0700

    test

---
+++ Scripts/Verify Correct Alarm Message/Script1529986178839.groovy
@@ -0,0 +1,55 @@
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import ChangeAndroidPasscode as AndroidPasscode
+import com.kms.katalon.core.util.internal.PathUtil as PathUtil
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory as MobileDriverFactory
+import io.appium.java_client.AppiumDriver as AppiumDriver
+import io.appium.java_client.MobileElement as MobileElement
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+
+Mobile.comment('Story: Verify correct alarm message')
+
+Mobile.comment('Given that user has started an application')
+
+'Get full directory\'s path of android application'
+def appPath = PathUtil.relativeToAbsolutePath(GlobalVariable.G_AppPath, RunConfiguration.getProjectDir())
+
+Mobile.startApplication(appPath, false)
+
+CustomKeywords.'ChangeAndroidPasscode.createPasscode'('io.appium.android.apis')
+
+Mobile.comment('And he navigates the application to Activity form')
+
+Mobile.tap(findTestObject('Application/android.widget.TextView - App'), 10)
+
+Mobile.tap(findTestObject('Application/App/android.widget.TextView - Activity'), 10)
+
+Mobile.comment('When he taps on the Custom Dialog button')
+
+Mobile.tap(findTestObject('Application/App/Activity/android.widget.TextView - Custom Dialog'), 10)
+
+'Get displayed message on the dialog'
+def message = Mobile.getText(findTestObject('Application/App/Activity/Custom Dialog/android.widget.TextView - Message'), 
+    10)
+
+Mobile.comment('Then the correct dialog message should be displayed')
+
+Mobile.verifyEqual(message, 'Example of how you can use a custom Theme.Dialog theme to make an activity that looks like a customized dialog, here with an ugly frame.')
+
+Mobile.closeApplication()
+
+
